aa52c37f646b4bcd82e7b9c4adfe56c181444b0a
morachake <jacobmoracha@gmail.com>
2025-01-21 12:54:54 +0300

    move sponsorship and additional package data to external file for better maintainability

--- src/utilis/data.ts
+++ src/utilis/data.ts
@@ -45,3 +45,57 @@ export  const navigation = [
       available: true
     }
   ];
+
+ export  const sponsorshipPackages = [
+    {
+      name: 'Platinum',
+      price: 'KES ',
+      benefits: [
+        'Exclusive keynote acknowledgment',
+        'Prime logo placement on all event materials, website, and signage',
+        'Dedicated booth in the main foyer',
+        '10 complimentary passes for your team',
+        'Brand inclusion on attendee lanyards and promotional items',
+        'Opportunity for a 30-minute technical talk reviewed by our organizing team',
+        '4 Slots Available',
+      ],
+    },
+    {
+      name: 'Gold',
+      price: 'KES ',
+      benefits: [
+        'Acknowledgment during keynotes',
+        'Logo placement on all event materials and website',
+        'Dedicated booth in a prominent location',
+        '5 complimentary passes for your team',
+        'Shout-outs on event social media channels',
+        '6 Slots Available',
+      ],
+    },
+    {
+      name: 'Silver',
+      price: 'KES ',
+      benefits: [
+        'Logo placement on the event website and materials',
+        'Acknowledgment during keynotes',
+        '3 complimentary passes for your team',
+        'Social media mentions',
+        '8 Slots Available',
+      ],
+    },
+  ]
+
+
+export const additionalPackages = [
+  {
+    name: 'Additional Opportunities',
+    price: 'Custom Pricing',
+    benefits: [
+      'Networking Coffee Break - KES ',
+      'Lunch Sponsorship - KES (Includes branded materials on tables)',
+      'Attendee Swag - KES  (Water bottles, notebooks, branded gifts)',
+      'Workshop Sponsorship - KES  (Includes branding in workshop areas)',
+      'T-Shirts - KES ',
+    ],
+  },
+]
